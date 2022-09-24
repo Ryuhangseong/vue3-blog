ce2f688ebe32d99c3c00fd0e9064f77fc24b8c90
ryuhangseong <liuhangcheng2002@gmail.com>
2022-09-24 17:23:13 +0800

    update to 0.0.4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { ElButton } from 'element-plus'
+import 'element-plus/es/components/button/style/css'
+
 </script>
 
 <template>
@@ -9,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <el-button>Click here!</el-button>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
+/*
+ * @Author: ryuhangseong liuhangcheng2002@gmail.com
+ * @Date: 2022-09-21 21:22:31
+ * @LastEditors: ryuhangseong liuhangcheng2002@gmail.com
+ * @LastEditTime: 2022-09-24 17:10:01
+ * @FilePath: \vue3-blog\vite.config.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by ryuhangseong liuhangcheng2002@gmail.com, All Rights Reserved. 
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
